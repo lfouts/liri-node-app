bf0f7ee115c9a502605e0dbbd2e79c993f4d37fc
lfouts <lorenfouts@yahoo.com>
2018-05-22 16:24:31 -0400

    deleted some extra unnecessary key stuff

--- keys.js
+++ keys.js
@@ -1,15 +1,15 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: process.env.mNQBRN7aAqZp9RV6aqtocD72K,
-  consumer_secret: process.env.RRrEa6Xlov18dkyxG3pgPIXFJV8YnjdwkyCjHV65TamfKHE6R3,
-  access_token_key: process.env.995322690490765312-joCkHroXNcbJycAyHya3rwJiUrr0lt2,
-  access_token_secret: process.env.2Fq211235BquA8P0fxMQ07N9krfsikYL4fgSNLb3tD9GN
+  consumer_key: "mNQBRN7aAqZp9RV6aqtocD72K",
+  consumer_secret: "RRrEa6Xlov18dkyxG3pgPIXFJV8YnjdwkyCjHV65TamfKHE6R3",
+  access_token_key: "995322690490765312-joCkHroXNcbJycAyHya3rwJiUrr0lt2",
+  access_token_secret: "2Fq211235BquA8P0fxMQ07N9krfsikYL4fgSNLb3tD9GN"
 };
 
 var spotifyKeys = {
-  id: process.env.b4a03868365d4f18ade5cf789fc6bd89,
-  secret: process.env.dd92613e3e524602b42b4c711219c053
+  id: "b4a03868365d4f18ade5cf789fc6bd89",
+  secret: "dd92613e3e524602b42b4c711219c053"
 };
 
 module.exports = {twitterKeys, spotifyKeys};
